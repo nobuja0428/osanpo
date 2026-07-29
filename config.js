a4b35e0c3474fafbfd12aa7d73ed86fbdd5411be
nobuja0428 <kinobu014@gmail.com>
2026-07-29 15:43:24 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ window.OSANPO_CONFIG = {
   siteName: "おさんぽクラブ東京",
   siteSubtitle: "Tokyo Sanpo Club",
   basePath: "",
+  siteUrl: "",
   contactEmail: "",
   contactFormUrl: "",
   eventUpdateMode: "手動更新",
+  analytics: {
+    enabled: false,
+    provider: "ga4",
+    measurementId: "",
+  },
 };
